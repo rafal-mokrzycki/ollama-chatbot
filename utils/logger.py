ce4d5e38c56dbbda97caae1dda16e1eb26179460
Rafał Mokrzycki <rafalmokrzycki1@gmail.com>
2024-12-15 14:12:00 +0100

    Update custom logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,3 +1,8 @@
 class CustomLogger:
     def __init__(self, log_file_path):
         self.log_file_path = log_file_path
+
+    def write_logs(self, question, answer):
+        with open(self.log_file_path, "a", encoding="utf-8") as log_file:
+            log_file.write(f"User: {question}\n")
+            log_file.write(f"AI: {answer}\n")
